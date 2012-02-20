0b4956cb284fd8bd2b06815c7c07cde476e09293
Eric Loes <eric.loes@gmail.com>
2012-02-19 23:35:04 -0800

    Updated requirements and add webfaction settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -3,6 +3,8 @@ import os
 
 if 'EPIO' in os.environ:
     from .epio import *
+elif 'crazyweber' ==  os.environ['USER']:
+    from .webfaction import *
 else:
     try:
         from .local import *
