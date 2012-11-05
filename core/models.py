011d6ac9b48dc61a7174dd38b19b53ce100709c3
Eric Loes <eric.loes@gmail.com>
2012-11-04 16:42:16 -0800

    Added jquery cycle image plugin

--- core/models.py
+++ core/models.py
@@ -9,49 +9,49 @@ class NavigationNode(SlugMixin):
     hide = models.BooleanField()
     createdDate = models.DateTimeField(auto_now_add=True)
     slugValue = 'name'
-    
+
     def get_absolute_url(self):
         breadcrumb = self.get_breadcrumb()
-        
+
         url = "/"
 
         for crumb in breadcrumb:
             url += "%s/" % crumb.slug
-            
+
         if len(breadcrumb) > 1:
             url += "%i/" % self.id
 
         return url
-        
-            
+
+
     def get_breadcrumb(self):
         array = []
         self._get_breadcrumb_child(array)
-        
+
         nodes = []
         for node in array:
             if node:
                 nodes.append(node)
-        
+
         return nodes
-    
+
     def _get_breadcrumb_child(self, array):
         array.insert(0, self)
         if self.parentNode:
             array.append(self.parentNode._get_breadcrumb_child(array))
-            
+
     def _get_children(self):
         return NavigationNode.objects.filter(parentNode__id=self.id)
 
-    childNodes = property(_get_children)            
-            
+    childNodes = property(_get_children)
+
     class Meta:
         ordering = ('createdDate',)
         verbose_name_plural = 'Navigation Nodes'
 
     def __unicode__(self):
         return u"%s" % self.adminName
-        
+
 class Content(ImageMixin):
     navigationNode = models.ForeignKey('NavigationNode')
     name = models.CharField(max_length=50)
@@ -62,9 +62,9 @@ class Content(ImageMixin):
     createdDate = models.DateTimeField(auto_now_add=True)
     lastModified = models.DateTimeField(auto_now=True)
     slugValue = 'name'
-    
+
     class Meta:
         ordering = ('createdDate',)
-        
+
     def __unicode__(self):
         return u"%s" % self.name
--- core/views.py
+++ core/views.py
@@ -34,19 +34,12 @@ def second_level_page(request, parent_slug, slug, navigation_node_id, template_n
     secondaryNavigation = NavigationNode.objects.filter(parentNode__id=navigationNode.parentNode.id)
     thirdNavigation = NavigationNode.objects.filter(parentNode__id=navigationNode.id)
 
-    fourthNavigation = { }
-    for node in thirdNavigation:
-        fourthNavigationNodes = NavigationNode.objects.filter(parentNode__id=node.id)
-        if len(fourthNavigationNodes) > 0:
-            fourthNavigation[node.id] = fourthNavigationNodes
-
     contentItems = Content.objects.filter(
                             Q(navigationNode__parentNode__id=navigationNode.id) | Q(navigationNode__parentNode__parentNode__id=navigationNode.id)).order_by('-createdDate')[:5]
     return render_with_context(request, template_name, { 'navigationNode' : navigationNode,
                                                             'breadcrumb': navigationNode.get_breadcrumb(),
                                                             'secondaryNavigation' : secondaryNavigation,
                                                             'thirdNavigation' : thirdNavigation,
-                                                            'fourthNavigation': fourthNavigation,
                                                             'contentItems' : contentItems })
 
 # /education/library/technical-books/9/
